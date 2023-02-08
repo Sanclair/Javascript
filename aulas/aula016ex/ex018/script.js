a4529350c5bc0af3cd938e912c79c976545b88bc
Sanclair <121900581+Sanclair@users.noreply.github.com>
2023-02-08 19:36:02 -0300

    finalização do curso introdutório de JavaScript

--- aulas/aula016ex/ex018/script.js
+++ aulas/aula016ex/ex018/script.js
@@ -21,8 +21,42 @@ function inLista(n,l) {
 
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value,valores)) {
-        window.alert('Tudo ok!')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já digitado')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados!</p>`
+        res.innerHTML += `<p>O maior valor digitado é ${maior}</p>`
+        res.innerHTML += `<p>O menor valor digitado é ${menor}</p>`
+        res.innerHTML += `<p>A soma de todos os valores digitados é de ${soma}</p>`
+        res.innerHTML += `<p>A media dos valores digitados é de ${media}</p>`
     }
 }
